e732e767635b564e7cf85e23bfe523d0ad0c0fc8
Shahram Akbarinasaji <sakbarin@sfu.ca>
2019-10-01 17:32:42 -0700

    first version for polynomial_regression.py

--- assignment-1/polynomial_regression.py
+++ assignment-1/polynomial_regression.py
@@ -1,18 +1,15 @@
-#!/usr/bin/env python
-
 import assignment1 as a1
 import numpy as np
 import matplotlib.pyplot as plt
 
 (countries, features, values) = a1.load_unicef_data()
 
-#print("%s %f" % (countries[values[:,0].argmax()], values[:,0].max()))
-#print("%s %f" % (countries[values[:,1].argmax()], values[:,1].max()))
-
 targets = values[:,1]
 x = values[:,7:]
-#x = a1.normalize_data(x)
+x = a1.normalize_data(x)
 
+
+POLYNOMIAL_DEGREE = 6
 N_TRAIN = 100
 x_train = x[0:N_TRAIN,:]
 x_test = x[N_TRAIN:,:]
@@ -20,19 +17,24 @@ t_train = targets[0:N_TRAIN]
 t_test = targets[N_TRAIN:]
 
 
-# Complete the linear_regression and evaluate_regression functions of the assignment1.py
 # Pass the required parameters to these functions
+training_errors = {}
+test_errors = {}
 
-# (w, tr_err) = a1.linear_regression()
-# (t_est, te_err) = a1.evaluate_regression()
+for i in range(1, POLYNOMIAL_DEGREE + 1):
+    (w, tr_error) = a1.linear_regression(x_train, t_train, 'polynomial', 0, i)
+    training_errors[i] = tr_error
+
+    (t_est, te_err) = a1.evaluate_regression(x_test, t_test, w, i)
+    test_errors[i] = te_err
 
 
 # Produce a plot of results.
-plt.rcParams.update({'font.size': 15})
-#plt.plot(train_err.keys(), train_err.values())
-#plt.plot(test_err.keys(), test_err.values())
+plt.rcParams.update({'font.size': 12})
+plt.plot(list(training_errors.keys()), list(training_errors.values()))
+plt.plot(list(test_errors.keys()), list(test_errors.values()))
 plt.ylabel('RMS')
 plt.legend(['Training error','Testing error'])
 plt.title('Fit with polynomials, no regularization')
 plt.xlabel('Polynomial degree')
-#plt.show()
+plt.show()
